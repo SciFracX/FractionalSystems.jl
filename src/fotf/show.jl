f65cca75412cab5cb9c3a2e88fcabce1f83b549e
ErikQQY <2283984853@qq.com>
2022-01-12 22:55:02 +0800

    Change to another show method
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- src/fotf/show.jl
+++ src/fotf/show.jl
@@ -1,33 +1,23 @@
-Base.print(io::IO, G::FOTF) = show(io, G)
+Base.print(G::FOTF) = show(G)
 
-import Base.show
-
-function Base.show(io::IO, G::FOTF)
+function Base.show(G::FOTF)
     println(typeof(G))
     print("\n")
-    #=
-    for (i, _) in enumerate(G.num)
-        j=G.num[i]
-        l=G.nn[i]
-        if i == 1
-            if j == 1
-                print("s^{$l}")
-            else
-                print("$(showfirstsign(j))s^"*"{$l}")
-            end
-        else
-            print("$(showsign(j))s^"*"{$l}")
-        end
-    end
-    =#
-    print(polyshow(G.num, G.nn))
+    numstr = polyshow(G.num, G.nn)
+    denstr = polyshow(G.den, G.nn)
+    print(numstr)
     print("\n")
-    G.ioDelay == 0 ? println("-----"^max(length(G.den), length(G.num))) : println("-----"^max(length(G.den), length(G.num))*" exp($(G.ioDelay)*s)")
-    print(polyshow(G.den, G.nd))
+    maxlengthstr=length(max(numstr, denstr))
+    G.ioDelay == 0 ? println("-"^maxlengthstr) : println("-"^maxlengthstr*" exp($(G.ioDelay)*s)")
+    print(denstr)
 end
 
 
 function polyshow(p, np)
+    if typeof(p) <: Number
+        return p
+    end
+
     if length(np) == 0
         p=0
         np=0
@@ -35,11 +25,11 @@ function polyshow(p, np)
 
     ii = sortperm(np, rev=true)
     np = sort(np, rev=true)
-    P=""
-    p=p[ii]
+    P = ""
+    p = p[ii]
 
-    for i=1:length(p)
-        P=P*"+"*"$(p[i])"*"s^"*"{$(np[i])}"
+    for i = 1:length(p)
+        P = P*"+"*"$(p[i])"*"s^"*"{$(np[i])}"
     end
 
     P = P[2:end]
@@ -51,59 +41,4 @@ function polyshow(p, np)
     P = replace(P, "-1s" => "-s")
     
     return P
-end
-#=
-function fotfshow(G, key)
-    strN = polyshow(G.num, G.nn)
-    str = strN
-    strD = polyshow(G.den, G.nd)
-    nn = length(strN)
-    ns=0
-
-    if nn == 1 && strN == "0"
-        if key == 0
-            show(strN)
-        end
-    else
-        ns == length(strD)
-        nm = max(nn, ns)
-        if key == 0
-            show(" "^Int64(floor((nm-nn)/2))*"$(strN)")
-        end
-        ss=[]
-        T=G.ioDelay
-        if T > 0
-            ss="exp(-$(T)*s)"
-        end
-        if cmp(strD, "1") !=0
-            if T>0
-                str="("*"$str"*")*"*"$(ss)"*"/("*"$strD"
-            else
-                str="("*"$str"*")/("*"$strD"*")"
-            end
-            str=replace(replace(str, "{"=>""), "}"=>"")
-            if key == 0
-                show("-"^nm*"$ss")
-                show(" "^Int64(floor((nm-ns)/2))*"$strD")
-            end
-        end
-    end
-    show(str)
-end
-=#
-
-# Handling the signs of the polynomials's coefficients
-function showsign(a)
-    if a > 0
-        return " + "*"$a"
-    elseif a < 0
-        return " - "*"$(abs(a))"
-    end
-end
-function showfirstsign(a)
-    if a > 0
-        return "$a"
-    elseif a < 0
-        return "-"*"$(abs(a))"
-    end
 end
