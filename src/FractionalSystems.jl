43fad9d1a9f7e56690018875f912e55acccfd615
ErikQQY <2283984853@qq.com>
2023-12-23 19:08:59 +0800

    Remove FOLE for now
    Signed-off-by: ErikQQY <2283984853@qq.com>

--- src/FractionalSystems.jl
+++ src/FractionalSystems.jl
@@ -12,14 +12,11 @@ include("fotf/show.jl")
 include("fotf/rlocus.jl")
 include("fotf/freqresp.jl")
 
-include("FOLE.jl")
 
 export FOTF, fotf, fotfdata, base_order, fotf2cotf, rlocus, polyuniq, simplify, freqresp
 
 export polyshow
 
 export FOSS, foss, foss2ss
-
-export FOLyapunov
 
 end
